ce2c3e1b037318984d2164f6493c846a56e44fe4
phiraol <166308089+phiraol@users.noreply.github.com>
2025-10-20 18:06:18 +0300

    result

--- amazon_clone/src/Router.jsx
+++ amazon_clone/src/Router.jsx
@@ -5,18 +5,20 @@ import SingUp from './pages/auth/SingUp';
 import Order from './pages/orders/Order';
 import Payment from './pages/payment/Payment';
 import Cart from './pages/cart/Cart';
+import Results from './pages/results/Results';
 function Routing() {
     return (
       <Router>
         <Routes>
-                <Route path='/' element={<Landing/>} />
-                <Route path='/auth' element={<SingUp />} />
-                <Route path='/orders' element={<Order />} />
-                <Route path='/payment' element={<Payment />} />
-                <Route path='/cart' element={<Cart/>}/>
+          <Route path="/" element={<Landing />} />
+          <Route path="/auth" element={<SingUp />} />
+          <Route path="/orders" element={<Order />} />
+          <Route path="/category/:categoryName" element={<Results/>} />
+          <Route path="/payment" element={<Payment />} />
+          <Route path="/cart" element={<Cart />} />
         </Routes>
       </Router>
-  )
+    );
 }
 
 export default Routing
--- amazon_clone/src/pages/results/Results.jsx
+++ amazon_clone/src/pages/results/Results.jsx
@@ -1,10 +1,36 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import LayOut from '../../components/LayOut/LayOut'
 import classes from './result.module.css'
+import { useParams } from 'react-router-dom'
+import axios from 'axios'
+import { productUrl } from '../../api/api'
+import ProductCard from '../../components/Product/ProductCard'
 function Results() {
+  const [results, setResults] = useState ([]);
+  const { categoryName } = useParams();
+  useEffect(() => {
+      axios
+        .get(`${productUrl}/products/category/${categoryName}`)
+        .then((res) => {
+          setResults(res.data);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+  }, [])
   return (
-    <LayOut>results</LayOut>
-  )
+    <LayOut>
+      <section>
+        <h1 style={{ padding: "30px" }}>Results</h1>
+        <p style={{ padding: "30px" }}> category / {categoryName}</p>
+        <div className={classes.product_container}>
+          {results.map((product) => {
+            return <ProductCard product={product} key={product.id} />;
+          })}
+        </div>
+      </section>
+    </LayOut>
+  );
 }
 
 export default Results
