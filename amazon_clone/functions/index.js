ac37b33dbd575fd53247196305a7fff4b76e2e1f
phiraol <166308089+phiraol@users.noreply.github.com>
2025-12-13 07:44:53 +0300

    function

---
+++ amazon_clone/functions/index.js
@@ -0,0 +1,43 @@
+
+
+const {setGlobalOptions} = require("firebase-functions");
+const {onRequest} = require("firebase-functions/https");
+const logger = require("firebase-functions/logger");
+const express = require("express")
+const cors = require("cors")
+const dotenv = require("dotenv")
+dotenv.config();
+const stripe = require("stripe")(process.env.STRIPE_KEY);
+const app = express()
+app.use((cors({ origin: true })))
+app.use(express.json())
+
+app.get("/", (req, res) => {
+    res.status(200).json({
+        message:"Success !"
+    })
+})
+
+app.post("/payment/create", async (req, res) => {
+  const total = req.query.total;
+
+  if (total > 0) {
+    const paymentIntents = await stripe.paymentIntents.create({
+      amount: total,
+      currency: "usd",
+    });
+      res.status(201).json({
+        cleintSecret: paymentIntents.client_secret,
+      });
+  } else {
+    res.status(401).json({
+      message: "total must be greater than 0",
+    });
+  }
+});
+
+exports.api = onRequest(app)
+
+setGlobalOptions({ maxInstances: 10 });
+
+
