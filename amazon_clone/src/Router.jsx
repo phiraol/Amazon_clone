f98f36ade36e7a1a6330269b93a3e400f3483081
phiraol <166308089+phiraol@users.noreply.github.com>
2025-12-15 09:46:38 +0300

    payment setup

--- amazon_clone/src/Router.jsx
+++ amazon_clone/src/Router.jsx
@@ -7,6 +7,12 @@ import Payment from './pages/payment/Payment';
 import Cart from './pages/cart/Cart';
 import Results from './pages/results/Results';
 import ProductDetail from './pages/productDetail/ProductDetail';
+import { Elements } from "@stripe/react-stripe-js";
+import { loadStripe } from "@stripe/stripe-js";
+
+const stripePromise = loadStripe(
+  "pk_test_51ScumH33zoqqyPTluVAcoonR2sE0z8l3mDKNAc2bv1WivRLYQXOjBpEX96X3co6bwYDN4GB56pD7t7HbrpPJeE0L001TX8wLi6"
+);
 
 function Routing() {
     return (
@@ -17,7 +23,9 @@ function Routing() {
           <Route path="/orders" element={<Order />} />
           <Route path="/category/:categoryName" element={<Results />} />
           <Route path ="/products/:productId" element ={<ProductDetail/>}/>
-          <Route path="/payment" element={<Payment />} />
+          <Route path="/payment" element= {<Elements stripe={stripePromise}>
+      <Payment />
+    </Elements>} />
           <Route path="/cart" element={<Cart />} />
         </Routes>
       </Router>
--- amazon_clone/src/pages/payment/Payment.jsx
+++ amazon_clone/src/pages/payment/Payment.jsx
@@ -1,10 +1,81 @@
-import React from 'react'
+import React, { useContext, useState } from 'react'
 import LayOut from '../../components/LayOut/LayOut'
 import classes from './payment.module.css'
+import { DataContext } from '../../components/dataProvider/DataProvider'
+import ProductCard from "../../components/Product/ProductCard"
+import {
+  useStripe,
+  useElements,
+  CardElement
+} from "@stripe/react-stripe-js";
+import CurrancyFormat from '../../components/CurrancyFormat/CurrancyFormat'
+
+
 function Payment() {
+  const [{ basket, user }] = useContext(DataContext)
+    const totalItem = basket?.reduce((amount, item) => {
+      return item.amount + amount;
+    }, 0);
+  
+    const total = basket.reduce((amount, item) => {
+      return item.price * item.amount + amount;
+    }, 0);
+  // console.log(basket)
+  const[cardError, setCardError]= useState(null)
+    const stripe = useStripe();
+  const elements = useElements();
+  const handleChange = (e) => {
+    console.log(e)
+    e?.error?.message ? setCardError(e?.error?.message) : setCardError("");
+    
+  }
   return (
-    <LayOut>payment</LayOut>
-  )
+    <LayOut>
+      <div className={classes.payment_header}>Checkout ({totalItem}) items</div>
+      <section className={classes.payment}>
+        <div className={classes.flex}>
+          <h3>Delivery Address</h3>
+          <div>
+            <div>{user?.email}</div>
+            <div>123 React Lane</div>
+            <div>Chicago, IL</div>
+          </div>
+        </div>
+        <hr />
+        <div className={classes.flex}>
+          <h3>Review items and delivery</h3>
+          <div className={classes.review}>
+            {basket?.map((item) => (
+              <ProductCard product={item} flex={true} renderDes={true} />
+            ))}
+          </div>
+        </div>
+        <hr />
+        <div className={classes.flex}>
+          <h3>Payment method</h3>
+          <div className={classes.payment_card_container}>
+            <div className={classes.payment_details}>
+              <form action="">
+                {cardError && (
+                  <small style={{ color: "red" }}>{cardError}</small>
+                )}
+                <CardElement onChange={handleChange} />
+                <div className={classes.payment_price}>
+                  <div>
+                    <span style={{display:"flex", gap:"5px"}}>
+                      <p>Total Order ({totalItem}) |</p> 
+                      <CurrancyFormat amount={total} />
+                    </span>
+                  </div>
+                  <button>pay now</button>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </section>
+    </LayOut>
+  );
 }
 
 export default Payment
